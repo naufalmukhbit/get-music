3b2911732f6d7c397b2b7461408fd1e27a951544
Muhammad Naufal Mukhbit Amrullah <muhammad.naufal@ai.astra.co.id>
2022-04-02 10:40:03 +0700

    Fix API

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://ws.audioscrobbler.com/2.0";
+export const BASE_URL = "https://ws.audioscrobbler.com/2.0";
 
 export const API_KEY = "60ec849e257362d8fae554292de3f006";
